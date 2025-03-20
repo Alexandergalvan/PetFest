43de44b44c02c63243673800fc0d87b12a39eec8
Alexandergalvan <escoba.abanico@gmail.com>
2025-03-20 11:40:00 -0500

    n

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/PetFest/',
+  server: {
+    allowedHosts: ['d99b-187-136-170-44.ngrok-free.app'],
+  }
 })
